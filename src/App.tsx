52eaf565dc7db074b8929689393d342c15e69ec4
Gagandeep <gagan.ddun@gmail.com>
2023-02-03 21:40:03 +1100

    get initial solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+    .then(res => res.json())
+    .then(data => {
+      const randomSolution = data[Math.floor(Math.random() * data.length)]
+      setSolution(randomSolution.word)
+    })
+  }, [])
+
   return (
     <div className="App">
       <h1>Wordle (lingo)</h1>
+      {solution && <div>{solution}</div>}
     </div>
   )
 }
