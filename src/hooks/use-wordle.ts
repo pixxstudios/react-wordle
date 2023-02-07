a4a61f085ab9a889740513bed982f5905d10ff36
Gagandeep <gagan.ddun@gmail.com>
2023-02-07 19:38:55 +1100

    useWordle skeleton setup

--- src/hooks/use-wordle.ts
+++ src/hooks/use-wordle.ts
@@ -1,3 +1,23 @@
+import { useState } from "react"
+
 export const useWordle = (solution: string) => {
-    
+    const [turn, setTurn] = useState(0)
+    const [currentGuess, setCurrentGuess] = useState('')
+    const [guesses, setGuesses] = useState([]) // guesses as formatted array
+    const [history, setHistory] = useState([]) // gusses as string
+    const [isCorrect, setIsCorrect] = useState(false)
+
+    const formatGuess = () => {}
+
+    const addNewGuess = () => {}
+
+    const handleKeyup = () => {}
+
+    return {
+        turn,
+        currentGuess,
+        guesses,
+        isCorrect,
+        handleKeyup
+    }
 }
