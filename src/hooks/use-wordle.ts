64d553b548c05bf341661194a58814e45da50100
Gagandeep <gagan.ddun@gmail.com>
2023-02-08 21:56:23 +1100

    format guessed word

--- src/hooks/use-wordle.ts
+++ src/hooks/use-wordle.ts
@@ -8,7 +8,32 @@ export const useWordle = (solution: string) => {
     const [isCorrect, setIsCorrect] = useState(false)
 
     const formatGuess = () => {
-        console.log('formatting ', currentGuess)
+        console.log('formatting ', currentGuess, solution)
+        const solutionArray: Array<string|null> = [...solution]
+        const formattedGuess = [...currentGuess].map(letter => {
+            return ({
+                key: letter,
+                color: 'grey'
+            })
+        })
+
+        // find any green letters
+        formattedGuess.forEach((letter, index) => {
+            if (solutionArray[index] === letter.key) {
+                formattedGuess[index].color = 'green'
+                solutionArray[index] = null;
+            }
+        })
+
+        // find any yellow letters
+        formattedGuess.forEach((letter, index) => {
+            if (solutionArray.includes(letter.key) && letter.color !== 'green') {
+                formattedGuess[index].color = 'yellow'
+                solutionArray[solutionArray.indexOf(letter.key)] = null
+            }
+        })
+
+        return formattedGuess
     }
 
     const addNewGuess = () => {}
@@ -16,7 +41,8 @@ export const useWordle = (solution: string) => {
     const handleKeyup = (e: KeyboardEvent) => {
         if (e.key === 'Enter') {
             if(turn > 5 || history.includes(currentGuess) || currentGuess.length !== 5) return;
-            formatGuess()
+            const formatted = formatGuess()
+            console.log(formatted)
         }
 
         if(e.key === 'Backspace') {
