58855078e07394d8a2a37f2e6051c0fdc79498e3
Gagandeep <gagan.ddun@gmail.com>
2023-02-22 21:43:37 +1100

    modal final

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -6,8 +6,23 @@ type Props = {
     solution: string
 }
 
-export const  Modal = ({ isCorrect }: Props) => {
+export const  Modal = ({ isCorrect, turn, solution }: Props) => {
   return (
-    <div>Modal</div>
+    <div className='modal'>
+        {isCorrect && (
+            <div>
+                <h1>You Win!</h1>
+                <p className='solution'>{solution}</p>
+                <p>You found the solution in {turn} guesses</p>
+            </div>
+        )}
+         {!isCorrect && (
+            <div>
+                <h1>Oops!</h1>
+                <p className='solution'>{solution}</p>
+                <p>Better luck next time</p>
+            </div>
+        )}
+    </div>
   )
 }
